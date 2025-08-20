64f4e7e44b23c14829640b00bef54d9bc549c659
Arshelle9912 <arshelleraheja@gmail.com>
2025-08-20 18:20:04 +0800

    Level 0: rename Duke to SonOfAnton and add greet/exit
    * The Duke class has been renamed to SonOfAnton to personalize the ChatBot.
    * Minimal Skeleton has been implemented to print a greet and exit message.

---
+++ src/main/java/SonOfAnton.java
@@ -0,0 +1,15 @@
+public class SonOfAnton {
+    public static void main(String[] args) {
+        String logo = "    _    _   _ _______  ____  _   _ \n"
+                + "   / \\  | \\ | |__   __|/ __ \\| \\ | |\n"
+                + "  / _ \\ |  \\| |  | |  | |  | |  \\| |\n"
+                + " / ___ \\| |\\  |  | |  | |__| | |\\  |\n"
+                + "/_/   \\_\\_| \\_|  |_|   \\____/|_| \\_|\n";
+        System.out.println("Hello from Son of\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
